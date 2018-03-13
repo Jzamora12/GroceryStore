cf5d57cb76a15097e5a24a6abe3b33c73eb7b301
Jzamora12 <jzamora12@mail.csuchico.edu>
2018-03-13 11:28:09 -0700

    uses an vector now.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,43 @@
 //Author: Joseluis Zamora
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-string list[5]; //array of 5 strings 
-int numItems = 0; 
-char input;
-  string item;
- do {
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
-  if ( input == 'a' || input == 'A' )
-  {  
-  cout<<"What is the item?\n"; 
-   cin >> item;
-   if ( numItems < 5)
-   {
-   list[numItems] = item; 
-   numItems++; 
-   }
-  else
-  
-  cout<<"You'll need a bigger list!\n";
-  }
- } while ( input != 'Q' && input != 'q' );
-    cout<<"==ITEMS TO BUY==\n";
-  for (int i = 0; i < 5; i++) 
-    {
-        cout<<i+1; 
-        cout<<" "<<list[i]<<endl;
-    }
+    vector <string> list; //creates vecoter for the grocery list. 
+    int numItems = 0; 
+    char input;
+    string item;
+    list.clear(); 
+    do 
+        {
+            cout<<"\n==GROCERY LIST MANAGER==";
+            cout<<"\nEnter your choice: ";
+            cout<<"\n (A)dd an item";
+            cout<<"\n (Q)uit";
+            cout<<"\nYour choice (A/Q): ";
+            cin>>input;
+            if ( input == 'a' || input == 'A' )
+                {  
+                    cout<<"What is the item?\n"; 
+                    cin >> item;
+                    list.push_back(item); 
+                }   
+    } while ( input != 'Q' && input != 'q' );
+        if ( !list.empty())
+            {
+                cout<<"==ITEMS TO Buy==\n";
+                for (int i = 0; i < list.size() ; i++) 
+                    {
+                        cout<<i+1; 
+                        cout<<" "<<list[i]<<endl;
+                    }
+            }       
+        else 
+            {
+                cout<< "No items to buy!";
+            }
   return 0;
 }
